302ee9f8ae3d9c552e9c4c7a2acae55cbf7f6092
rohanbandha <rohansibuna@gmail.com>
2025-10-09 23:50:04 +0530

    Primitive

---
+++ jsfoundation/part2/nonPrimitives.js
@@ -0,0 +1,21 @@
+const user = {
+    firstname: "hitesh",
+    isLoggedin: true
+}
+
+user.firstname = "Mr. Sibuna"
+user.lastname= "Bandha"
+
+console.log(user.firstname);
+console.log(user.lastname);
+
+console.log(typeof user);
+
+let today = new Date;
+console.log(today.getDate());
+
+let heros = ["hitesh", "b", true]
+console.log(heros[0]);
+
+let isValue = true;
+console.log(isValue + 1);
