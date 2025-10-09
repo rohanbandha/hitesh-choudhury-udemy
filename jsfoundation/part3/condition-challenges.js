2a15713ad9315ec30cbc5b5b8664ce21b9d8c0de
rohanbandha <rohansibuna@gmail.com>
2025-10-10 00:10:50 +0530

    condition challenges

---
+++ jsfoundation/part3/condition-challenges.js
@@ -0,0 +1,46 @@
+let num1 = 5
+let num2 = 8
+console.log("I am regulad upper code");
+
+if (num1 > num2) {
+    console.log("num 1 is greater than nunm2");
+} else {
+    console.log("nope, num 1 is not greater");
+    
+}
+console.log("I am regulr bottom code");
+
+let username = "chai"
+let anotherUserName = "chai"
+
+if (username == anotherUserName) {
+    console.log("Pick another username");
+} else {
+    console.log("Ypu can choose another user name");
+}
+
+let score = "44"
+
+if (typeof score === 'number') {
+   console.log("yep, this is a number");
+} else {
+    console.log("No that is not a number");
+    
+}
+
+let isTeaReady =false
+
+if (isTeaReady) {
+    console.log("Tea is ready");    
+} else {
+    console.log("Tea is not ready");
+}
+
+let  item = [];
+console.log(item.length);
+
+if (item.length === 0) {
+    console.log("Array is empty");
+} else {
+    console.log("Array is not empty");
+}
