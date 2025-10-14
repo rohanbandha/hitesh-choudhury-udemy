8cb8689747da61986a7b4bb3bc9325a9e64a2cfa
rohanbandha <rohansibuna@gmail.com>
2025-10-14 23:51:47 +0530

    Functional constructors and Errors

---
+++ jsfoundation/part6/constructorFunction.js
@@ -0,0 +1,47 @@
+function greet(name) {
+    this.name = name
+    this.age = age
+}
+
+function car(make, model) {
+    this.make = make
+    this.model = model
+}
+
+let myCar = new car("Toyota", "camry");
+//console.log(myCar);
+
+let myNewCar = new car("Tata", "Safari")
+//console.log(myNewCar);
+
+function Tea(type) {
+    this.type = type
+    this.describe = function(){
+        return `this is a cup of ${this.type}`
+    }
+    
+}
+let lemonTea = new Tea("Lemon tea")
+console.log(lemonTea.describe());
+
+function Animal(species) {
+    this.species = species
+}
+Animal.prototype.sound = function(){
+    return `${this.species} makes a sound`
+}
+let dog = new Animal("Dog")
+console.log(dog.sound());
+
+let cat = new Animal("Cat")
+console.log(cat.sound());
+
+
+function Drink(name) {
+    if (!new.target) {
+        throw new Error("Drink must be called with new keyword")
+    }
+    this.name=name;
+}
+let tea = new Drink("tea");
+let coffee = Drink("coffee")
