2141787709bd2be417772c75ab25368bdfe3d964
rohanbandha <rohansibuna@gmail.com>
2025-10-18 20:15:50 +0530

    Dom finale with 5 more challenges

--- jsfoundation/part8/script.js
+++ jsfoundation/part8/script.js
@@ -38,3 +38,41 @@ document.getElementById("removeLastTask").addEventListener('click',function(){
     let taskList = document.getElementById("taskList");
     taskList.lastElementChild.remove();
 })
+
+//example 6
+
+document.getElementById("clickMeButton").addEventListener('dblclick', function(){
+    alert("chaicode")
+})
+
+//example 7
+
+document.getElementById("teaList").addEventListener('click', function(event){
+    console.log(event.target);
+    if (event.target && event.target.matches('.teaItem')) {
+        alert("You selected: " + event.target.textContent)
+    }
+})
+
+//example 8
+
+document.getElementById("feedbackForm").addEventListener('submit', function(event){
+    event.preventDefault();
+    let feedback = document.getElementById("feedbackInput").value
+    console.log(feedback);
+    document.getElementById("feedbackDisplay").textContent = ` Feedback is: ${feedback}`
+    
+})
+
+//example 9
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('domStatus').textContent = "DOM fully loaded"
+})
+
+//example 10
+
+document.getElementById("toggleHighlight").addEventListener('click',function(){
+    let descriptionText = document.getElementById("descriptionText")
+    descriptionText.classList.toggle('highlight')
+})
