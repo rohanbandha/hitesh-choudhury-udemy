5eb876845b31e3afdf86035b4d070eabf9b8992e
rohanbandha <rohansibuna@gmail.com>
2025-10-19 21:26:03 +0530

    closures in js

---
+++ jsfoundation/part9_adv/explore1.js
@@ -0,0 +1,15 @@
+function sayHello() {
+    console.log("I would like to say hello");
+}
+
+setTimeout(() => {
+    sayHello();
+}, 4000);
+
+console.log("Chaicode");
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+    
+    
+}
